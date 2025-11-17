4b8e062743666811f193f1f4cfed15ed242e3e01
Nicolo <nicolofrancis.gabiana>
2025-11-17 22:14:01 +0800

    Slight modifications to App.js

--- frontend/onlyfunds_frontend/src/App.js
+++ frontend/onlyfunds_frontend/src/App.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import Homepage from './pages/Homepage';
-import Register from './pages/Register';
-import Login from './pages/Login';
-
-function App() {
-  return (
-    <div className="App">
-      {/* <Login/> */}
-      {/* <Register/> */}
-=======
 import ScrollProgressBar from './pages/ScrollProgressBar';
 import Homepage from './pages/Homepage';
 import DonationCarousel from './pages/DonationCarousel';
@@ -17,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <ScrollProgressBar/>
->>>>>>> 5defb87b7a932ebd221e977216034558a354d5bc
       <Homepage/>
       <DonationCarousel/>
       <Footer/>
